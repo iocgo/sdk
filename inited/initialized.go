de20e9d96df01a9237df48fad5db2a90892ba2dc
bincooo <bincooo@bincooos-iMac.local>
2024-12-13 06:13:50 +0800

    update

--- inited/initialized.go
+++ inited/initialized.go
@@ -13,13 +13,22 @@ var (
 
 func AddInitialized(apply func(args ...interface{})) { inits = append(inits, apply) }
 func AddExited(apply func(args ...interface{}))      { exits = append(exits, apply) }
-func Initialized(args ...interface{}) {
+func Initialized(block bool, args ...interface{}) {
 	for _, apply := range inits {
 		apply(args...)
 	}
 
 	osSignal := make(chan os.Signal, 1)
 	signal.Notify(osSignal, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
+	if block {
+		<-osSignal
+		for _, apply := range exits {
+			apply(args...)
+		}
+		os.Exit(0)
+		return
+	}
+
 	go func(ch chan os.Signal) {
 		<-ch
 		for _, apply := range exits {
