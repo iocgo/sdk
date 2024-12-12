a39f7c05468f10e4a367e3e7b254f479794a7746
bincooo <bincooo@bincooos-iMac.local>
2024-12-13 05:27:31 +0800

    feat: add inited api

---
+++ inited/initialized.go
@@ -0,0 +1,30 @@
+package inited
+
+import (
+	"os"
+	"os/signal"
+	"syscall"
+)
+
+var (
+	inits = make([]func(args ...interface{}), 0)
+	exits = make([]func(args ...interface{}), 0)
+)
+
+func AddInitialized(apply func(args ...interface{})) { inits = append(inits, apply) }
+func AddExited(apply func(args ...interface{}))      { exits = append(exits, apply) }
+func Initialized(args ...interface{}) {
+	for _, apply := range inits {
+		apply(args)
+	}
+
+	osSignal := make(chan os.Signal, 1)
+	signal.Notify(osSignal, syscall.SIGINT, syscall.SIGTERM, syscall.SIGKILL)
+	go func(ch chan os.Signal) {
+		<-ch
+		for _, apply := range exits {
+			apply(args)
+		}
+		os.Exit(0)
+	}(osSignal)
+}
